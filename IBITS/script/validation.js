d9db8ebd8b7aaea7539af7cbada190209bf0a8f1
jasperjacoob <68730598+jasperjacoob@users.noreply.github.com>
2021-02-07 23:50:07 +0800

    inayos ko lang may mga alert na den sa register

---
+++ IBITS/script/validation.js
@@ -0,0 +1,26 @@
+function validate() {
+
+    var email = document.getElementById('email');
+    var lastName = document.getElementById('last_name');
+    var firstName = document.getElementById('first_name');
+    var middleName = document.getElementById('middle_name');
+    var birthday = document.getElementById('birthday');
+    var course = document.getElementById('course');
+
+    if (email.value == "" || lastName.value == "" || firstName.value == "" || middleName.value == "" 
+         || birthday.value == "" || course.value == "COURSE"){
+        
+        swal ( "Oops" ,  "Please Complete Fields!" ,  "error" );
+        return false;
+    }
+    else{
+        swal("submitted!", "thankyouu!", "success");
+        return true;
+    }
+
+
+
+
+
+
+}
